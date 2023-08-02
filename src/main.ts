2c331db8b2f92a1ac082455aa0617d594939823d
jwz934738949 <934738949@qq.com>
2023-08-02 22:19:35 +0800

    feat: 引入element-plus插件

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@
 
 import { createApp } from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
